6ebd7349ed8a3a82b8b9403c79d47b02a2b37dd4
okuro4-dev <gassfod2@gmail.com>
2026-04-21 20:00:19 +0200

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Kuro AI",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
